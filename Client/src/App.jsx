733898c9851e4638af4569c9a21932d2417576cc
Kartik Bhardwaj <developer.kartikbhardwaj@gmail.com>
2024-09-23 09:34:53 +0530

    mime error handled

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,6 +6,21 @@ import ChatProvider from "./context/chatprovider";
 
 
 const App = () => {
+
+
+  const handleError = (error) => {
+    console.error("Resource loading failed:", error);
+  };
+  
+  useEffect(() => {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://chatease-frontend-4ips.onrender.com/css/blockedPage.css';
+    link.onload = () => console.log("Stylesheet loaded successfully");
+    link.onerror = handleError;
+    document.head.appendChild(link);
+  }, []);
+
   const router = createBrowserRouter([
     {
       path: "/",
